9b2835d9c3aadee3229bd106523db4b06fc9773c
superv88 <remanchim@gmail.com>
2020-02-17 16:59:02 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1') // use # to access id
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = 'Error: ' + data.error
+            } else {
+                messageOne.textContent = 'Location: ' + data.location
+                messageTwo.textContent = 'Forecast: ' + data.forecast 
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/20a925e100f97707e0fa10da5c47b7f7/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if(body.error) {
+            callback('Unable to find location! Try another search', undefined)
+        } else {
+            //callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
